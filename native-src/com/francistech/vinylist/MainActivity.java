1717719927b4f28f227056213d4bc0a43f044410
Franci6060m <121741264+Franci6060m@users.noreply.github.com>
2026-08-09 15:15:06 +0300

    Add files via upload

---
+++ native-src/com/francistech/vinylist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.francistech.vinylist;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(MediaStorePlugin.class);
+        registerPlugin(MediaControlPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
